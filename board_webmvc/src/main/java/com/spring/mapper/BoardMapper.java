0c24f23a1b213aecf421b9b040a40de939ca5a1d
Saejung <leeesaejung@gmail.com>
2023-05-18 18:00:55 +0900

    2023년 5월 18일 board_webmvc 기본적인 CRUD 작성과 페이지 넘기기

--- board_webmvc/src/main/java/com/spring/mapper/BoardMapper.java
+++ board_webmvc/src/main/java/com/spring/mapper/BoardMapper.java
@@ -3,10 +3,15 @@ package com.spring.mapper;
 import java.util.List;
 
 import com.spring.domain.BoardDTO;
-
+import com.spring.domain.Criteria;
+// 언제나 항상 mapper.xml과 짝꿍
 public interface BoardMapper {
 
-	public List<BoardDTO> list();
+	public List<BoardDTO> list(Criteria cri);
 	public int insert(BoardDTO dto);
+	public BoardDTO get(int bno);
+	public int update(BoardDTO dto);
+	public int delete(int bno);
+	public int totalCnt();
 
 }
---
+++ board_webmvc/src/main/webapp/resources/js/read.js
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+
+const form = document.querySelector("#openForm");
+
+// 수정 버튼 클릭 시 openForm 보내기
+// /board/modify 전송
+document.querySelector(".btn-info").addEventListener("click", () => {
+  form.action = "/board/modify";
+  form.submit();
+});
+
+// 목록 버튼 클릭 시
+// /board/list 전송
+// bno 제거
+document.querySelector(".btn-secondary").addEventListener("click", () => {
+  form.firstElementChild.remove();
+  form.action = "/board/list";
+  form.submit();
+});
