45b23d38e7a5b05b26367d9a1ac9fc1ed9e9319f
Saejung <leeesaejung@gmail.com>
2023-05-24 17:54:45 +0900

    2023년 5월 23일-24일 restController, board_webmvc 댓글 입력 완성 조회 미완

--- board_webmvc/src/main/java/com/spring/controller/BoardController.java
+++ board_webmvc/src/main/java/com/spring/controller/BoardController.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -112,5 +114,6 @@ public class BoardController {
 		return "redirect:/board/list";
 		
 	}
+
 	
 }
--- rest/src/main/java/com/spring/controller/HomeController.java
+++ rest/src/main/java/com/spring/controller/HomeController.java
@@ -6,10 +6,17 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.spring.domain.SampleDTO;
 
 /**
  * Handles requests for the application home page.
@@ -23,17 +30,32 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	public String home() {
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+		return "index";
 	}
 	
+	// produces : 리턴 값을 json 형태로 보낼 예정
+	// 자바 객체 ==> json 형태로 변경해주는 라이브러리가 필요함
+	@ResponseBody
+	@GetMapping(value="/test",produces = MediaType.APPLICATION_JSON_VALUE)
+	public SampleDTO sendDto() {
+		return new SampleDTO("1234", "홍", "길동");
+	}
+	
+	// ResponseEntity를 쓰면 @ResponseBody 를 안 써도 된다.
+	@GetMapping(value="/check1",produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<SampleDTO> check(double height, double weight) {
+		
+		SampleDTO dto = new SampleDTO("1234", ""+height, ""+weight);
+		
+		ResponseEntity<SampleDTO> entity = null;
+		
+		if (height < 160) {
+			entity = new ResponseEntity<SampleDTO>(dto, HttpStatus.BAD_REQUEST);
+		}else {
+			entity = new ResponseEntity<SampleDTO>(dto, HttpStatus.OK);
+		}
+		return entity;
+	}
 }
